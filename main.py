2829de42770943e65b67f95cf9fef4a5e90dca0c
Alex Hewitt <alexhewitt.j@gmail.com>
2024-07-01 07:53:02 -0500

    updates to initial screen setup

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        root = Tk()
-        root.title = "Maze Solver"
-        canvas = Canvas()
-        canvas.pack()
-        running = False
+        self.root = Tk()
+        self.root.title = "Maze Solver"
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
 
     def redraw(self):
         self.root.update_idletasks()
         self.root.update()
 
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()
