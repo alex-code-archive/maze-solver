464a52484fa1e0fbba976dcca4e3e68e4ac72065
Alex Hewitt <alexhewitt.j@gmail.com>
2024-07-01 08:00:54 -0500

    merge classes into one file

--- graphics.py
+++ graphics.py
@@ -23,3 +23,25 @@ class Window:
 
     def close(self):
         self.running = False
+
+
+class Point:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_1.x,
+            self.point_1.y,
+            self.point_2.x,
+            self.point_2.y,
+            fill=fill_color,
+            width=2,
+        )
