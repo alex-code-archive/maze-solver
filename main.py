cd4f143d7b9d226f82d5843947d5379c6bfc85df
Alex Hewitt <alexhewitt.j@gmail.com>
2024-07-02 21:17:04 -0500

    add maze creation

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from graphics import Window, Line, Point, Cell
+from maze import Maze
 
 
 def main():
     win = Window(800, 600)
-    c = Cell(50, 50, 100, 100, win)
-    c.has_left_wall = False
-    c.draw()
-
-    c1 = Cell(125, 125, 200, 200, win)
-    c1.has_right_wall = False
-    c1.draw()
-
-    c2 = Cell(225, 225, 250, 250, win)
-    c2.has_bottom_wall = False
-    c2.draw()
-    c2.draw_move(c1, True)
-
-    c = Cell(300, 300, 500, 500, win)
-    c.has_top_wall = False
-    c.draw()
-    print("made it")
+    # c = Cell(50, 50, 100, 100, win)
+    # c.has_left_wall = False
+    # c.draw()
+    #
+    # c1 = Cell(125, 125, 200, 200, win)
+    # c1.has_right_wall = False
+    # c1.draw()
+    #
+    # c2 = Cell(225, 225, 250, 250, win)
+    # c2.has_bottom_wall = False
+    # c2.draw()
+    # c2.draw_move(c1, True)
+    #
+    # c = Cell(300, 300, 500, 500, win)
+    # c.has_top_wall = False
+    # c.draw()
+    Maze(10, 10, 5, 5, 50, 50, win)
     win.wait_for_close()
 
 
---
+++ maze.py
@@ -0,0 +1,32 @@
+from graphics import Window, Line, Point, Cell
+
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self.num_rows + 1):
+            self.cells.append([])
+            for j in range(self.num_cols + 1):
+                cell = Cell(
+                    self.x1,
+                    self.y1,
+                    self.x1 + j * self.cell_size_x,
+                    self.y1 + i * self.cell_size_y,
+                    self.win,
+                )
+                self.cells[i].append(cell)
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        cell = self.cells[i][j]
+        cell.draw()
