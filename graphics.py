5dddf729681011c44f5ce2936f0d82d271907e27
Alex Hewitt <alexhewitt.j@gmail.com>
2024-07-03 08:42:16 -0500

    add break walls

--- graphics.py
+++ graphics.py
@@ -56,6 +56,7 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self.x1 = None
         self.x2 = None
         self.y1 = None
